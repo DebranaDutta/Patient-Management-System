67567e1361dc6959a0bcb5c3ef1eb6affd24ba53
debranadutta <debduttaother@gmail.com>
2026-05-22 19:00:06 +0530

    modification on api gateway

--- DcmApiGateway/src/main/java/com/agw/DcmApiGatewayApplication.java
+++ DcmApiGateway/src/main/java/com/agw/DcmApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.agw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DcmApiGatewayApplication {
 
 	public static void main(String[] args) {
