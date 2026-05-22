c5a7e7e7f45ca8f170f8d72d18221e6b742085f3
debranadutta <debduttaother@gmail.com>
2026-05-22 18:40:08 +0530

    api gateway

---
+++ DcmApiGateway/src/main/java/com/agw/DcmApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.agw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcmApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcmApiGatewayApplication.class, args);
+	}
+
+}
---
+++ DcmApiGateway/src/test/java/com/agw/DcmApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcmApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
